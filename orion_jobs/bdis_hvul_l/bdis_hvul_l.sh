c0d4f31c600f1c775f98060247effcb8e618824f
tonjeim <tonjeeeim@gmail.com>
2025-05-13 00:35:28 +0200

    Add files via upload

---
+++ orion_jobs/bdis_hvul_l/bdis_hvul_l.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+#SBATCH --nodes=1 
+#SBATCH --ntasks=10 
+#SBATCH --mem=90G 
+#SBATCH --time=01:00:00 
+#SBATCH --partition=RHEL9,smallmem,orion
+#SBATCH --job-name=R_analysis  
+#SBATCH --output=R_analysis_bdis_hvul%j.log 
+
+# Navigate to the scratch directory 
+cd /mnt/users/tonjeei/RScripts/bdis_hvul_l
+ 
+# Load R module 
+module load R 
+
+# Set the RSTUDIO_PANDOC environment variable 
+export RSTUDIO_PANDOC="$HOME/bin/pandoc"
+
+# Render the R Markdown file 
+Rscript -e "rmarkdown::render('bdis_hvul_l.Rmd')"
